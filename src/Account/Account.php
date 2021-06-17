95f77a27ce4f4b72a51006ca31ee2e5f55f72105
wyh <1579775085@qq.com>
2021-06-17 10:25:29 +0800

    新增 根据证件号查询账户id接口

--- src/Account/Account.php
+++ src/Account/Account.php
@@ -33,7 +33,7 @@ class Account extends AbstractApi
      *
      * @param $name
      * @param $organCode
-     * @param  string  $regType
+     * @param string $regType
      * @param $arr array
      * @return mixed|null
      * @throws \Exception
@@ -44,10 +44,31 @@ class Account extends AbstractApi
         $url = '/tech-sdkwrapper/timevale/account/addOrganize';
 
         $params = [
-            'name'      => $name,
-            'organCode' => $organCode,
-            'regType'   => $regType
-        ] + $arr;
+                'name'      => $name,
+                'organCode' => $organCode,
+                'regType'   => $regType
+            ] + $arr;
+
+        return $this->parseJSON('json', [$url, $params]);
+    }
+
+    /**
+     * 根据证件号查询账户ID
+     *
+     * @param $idNumber
+     * @param $idType
+     * @return false|mixed|null
+     * @throws \Exception
+     * @link https://open.esign.cn/doc/detail?id=opendoc%2Fpaas_sdk%2Fxoc3h2&namespace=opendoc%2Fpaas_sdk
+     */
+    public function getAccountInfoByIdNo($idNumber, $idType)
+    {
+        $url = '/tech-sdkwrapper/timevale/account/getAccountInfoByIdNo';
+
+        $params = [
+            'idNo'     => $idNumber,
+            'idNoType' => $idType
+        ];
 
         return $this->parseJSON('json', [$url, $params]);
     }
